b9d8e433bc219f8577a4fc52fea2c7d2bd922fb4
jectgenius <jkl9087@naver.com>
2024-03-19 18:50:38 +0900

    Feat: 2장62쪽 체크타임 3번

---
+++ _02_java_basic_programming/src/main/java/com/sh/checkTime/Hi.java
@@ -0,0 +1,7 @@
+package com.sh.checkTime;
+
+public class Hi {                               // Hi 클래스 정의 시작
+    public static void main(String[] args) {    // main() 메소드 정의 시작
+        System.out.println("Hi");               // System.out.println() 메소드 호출하여 문자열 "Hi" 출력
+    }                                           // main() 메소드 정의 끝
+}                                               // Hi 클래스 정의 끝
