f5fa86410020c1e584777cb8cddd331c6cea922c
jectgenius <jkl9087@naver.com>
2024-03-25 23:28:52 +0900

    Feat: 1장 오픈챌린지, 실습문제

---
+++ OpenChallenge1/src/main/java/MyInfoApp.java
@@ -0,0 +1,7 @@
+public class MyInfoApp {                                            // MyInfoApp 클래스 정의 시작
+    public static void main(String[] args) {                        // main() 메소드 정의 시작
+        System.out.println("Kitae Hwang");                          // System.out.println() 메소드 호출하여 문자열 "Kitae Hwang" 출력
+        System.out.println("20 years old");                         // System.out.println() 메소드 호출하여 문자열 "20 years old" 출력
+        System.out.println("Departmant of Computer Engineering");   // System.out.println() 메소드 호출하여 문자열 "Department of Computer Engineering" 출력
+    }                                                               // main() 메소드 정의 끝
+}                                                                   // MyInfoApp 클래스 정의 끝
