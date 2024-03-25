ca0b1c75d6f37d57ad9d0a35d0d0a60377c2c995
jectgenius <jkl9087@naver.com>
2024-03-25 23:07:54 +0900

    Feat: 1장 SampleProject

---
+++ SampleProject/src/main/java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {                            // Hello 클래스 정의 시작
+    public static void main(String[] args) {    // main() 메소드 정의 시작
+        int n = 2030;                           // int형 변수 n 선언 동시에 2030 대입하여 초기화
+        System.out.print("헬로" + n);            // System.out.print() 메소드 호출하여 문자열 "헬로2030" 출력
+    }                                           // main() 메소드 정의 끝
+}                                               // Hello 클래스 정의 끝
