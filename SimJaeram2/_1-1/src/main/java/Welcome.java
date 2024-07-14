33ea512dda827c1c9e33ddb5b3085bf1123c1a0e
jectgenius <jkl9087@naver.com>
2024-07-14 13:32:37 +0900

    Feat(SimJaeram2): 1장 자바 시작 실습문제 #13

---
+++ SimJaeram2/_1-1/src/main/java/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {                                          // Welcome 클래스 정의 시작
+    public static void main(String[] args) {                    // main() 메소드 정의 시작
+        System.out.println("Welcome!!");                        // System.out.println() 메소드 호출하여 문자열 "Welcome!!" 출력
+        System.out.println("자바 세계로 오신 것을 환영합니다.");     // System.out.println() 메소드 호출하여 문자열 "자바 세계로 오신 것을 환영합니다." 출력
+    }                                                           // main() 메소드 정의 끝
+}                                                               // Welcome 클래스 정의 끝
---
+++ SimJaeram2/_1-2/src/main/java/Sorry.java
@@ -0,0 +1,6 @@
+public class Sorry {                                // Sorry 클래스 정의 시작
+    public static void main(String[] args) {        // main() 메소드 정의 시작
+        System.out.println("Sorry~~");              // System.out.println() 메소드 호출하여 문자열 "Sorry~~" 출력
+        System.out.println("재밌어서 미안합니다~~");   // System.out.println() 메소드 호출하여 문자열 "재밌어서 미안합니다~~" 출력
+    }                                               // main() 메소드 정의 끝
+}                                                   // Sorry 클래스 정의 끝
