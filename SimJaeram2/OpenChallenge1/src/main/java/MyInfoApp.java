efe79f4adafba64823c6419acfc057c91859c96d
jectgenius <jkl9087@naver.com>
2024-07-14 13:33:23 +0900

    Feat(SimJaeram2): 1장 자바 시작 OpenChaellenge #13

---
+++ SimJaeram2/OpenChallenge1/src/main/java/MyInfoApp.java
@@ -0,0 +1,7 @@
+public class MyInfoApp {                                            // MyInfoApp 클래스 정의 시작
+    public static void main(String[] args) {                        // main() 메소드 정의 시작
+        System.out.println("Kitae Hwang");                          // System.out.println() 표준 출력 메소드 호출하여 문자열 "Kitae Hwang" 출력, 이름 출력
+        System.out.println("20 years old");                         // System.out.println() 표준 출력 메소드 호출하여 문자열 "20 years old" 출력, 나이 출력
+        System.out.println("Department of Computer Engineering");   // System.out.println() 표준 출력 메소드 호출하여 문자열 "Department of Computer Engineering" 출력, 학과 출력
+    }                                                               // main() 메소드 정의 끝
+}                                                                   // MyInfoApp 클래스 정의 끝
